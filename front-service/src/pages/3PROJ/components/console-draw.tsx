b2dabc1006d95898fa3cc7da0f349761ac58f2bd
DrNoProblem <maxverhaeghe@gmail.com>
2024-04-11 16:01:53 +0200

    3PROJ

--- front-service/src/pages/3PROJ/components/console-draw.tsx
+++ front-service/src/pages/3PROJ/components/console-draw.tsx
@@ -16,10 +16,22 @@ type Props = {};
 const ConsoleDrawComponent: FC<Props> = () => {
   const [ScriptValue, setScriptValue] = useState<string[]>();
 
-  let x = 225; // Position initiale x (au centre du canvas)
-  let y = 225; // Position initiale y (au centre du canvas)
-  let angle = 0; // Angle initial en degrés
+  let PosX = 225; // Position initiale x (au centre du canvas)
+  let PosY = 225; // Position initiale y (au centre du canvas)
+  let rotation = 0; // Angle initial en degrés
+  let draw = true;
+  let RocketVisible = true;
   const canvasRef = useRef<HTMLCanvasElement>(null);
+
+  const updateRocketPosition = () => {
+    const rocketElement = document.getElementById("cursor");
+    if (rocketElement) {
+      rocketElement.style.left = `${PosX}px`;
+      rocketElement.style.top = `${PosY}px`; // 450 est la hauteur du canvas, ajustez selon vos besoins
+      rocketElement.style.transform = `rotate(${-rotation}deg) translate(-50%, calc(-50% + 2px))`; // L'angle doit être négatif pour correspondre à la rotation dans le sens horaire
+      rocketElement.style.opacity = `${RocketVisible ? 1 : 0}`;
+    }
+  };
 
   useEffect(() => {
     if (canvasRef.current) {
@@ -27,83 +39,124 @@ const ConsoleDrawComponent: FC<Props> = () => {
       if (ctx) {
         ctx.fillStyle = "white";
         ctx.fillRect(0, 0, 450, 450); // Initialise le fond du canvas en blanc
-        ctx.moveTo(x, y);
+        ctx.moveTo(PosX, PosY);
       }
     }
   }, []);
 
-  const simulateTurtle = (command: ICommand, ctx: CanvasRenderingContext2D | null) => {
+  const simulateRocket = (command: ICommand, ctx: CanvasRenderingContext2D | null) => {
     if (!ctx) return;
 
     switch (command.command) {
       case "AV":
         console.log(`La tortue avance de ${command.params[0]} pixels`);
-        const distance = command.params[0];
-        const newX = x + distance * Math.cos((angle * Math.PI) / 180);
-        const newY = y + distance * Math.sin((angle * Math.PI) / 180);
-        ctx.beginPath(); // Commencer un nouveau chemin
-        ctx.moveTo(x, y); // Déplacer à la position actuelle (sans tracer)
-        ctx.lineTo(newX, newY); // Tracer une ligne jusqu'à la nouvelle position
-        ctx.stroke(); // Appliquer le tracé
-
-        // Mettre à jour la position de la tortue
-        x = newX;
-        y = newY;
+        const distance_AV = command.params[0];
+        const newX_AV = PosX - distance_AV * Math.sin((rotation * Math.PI) / 180);
+        const newY_AV = PosY - distance_AV * Math.cos((rotation * Math.PI) / 180);
+        ctx.beginPath();
+        ctx.moveTo(PosX, PosY);
+        ctx.lineTo(newX_AV, newY_AV);
+        ctx.stroke();
+        PosX = newX_AV;
+        PosY = newY_AV;
+        updateRocketPosition();
         break;
       case "RE":
-        console.log(`La tortue recule de ${command.params[0]} pixels`);
+        console.log(`La tortue avance de ${command.params[0]} pixels`);
+        const distance_RE = command.params[0];
+        const newX_RE = PosX + distance_RE * Math.sin((rotation * Math.PI) / 180);
+        const newY_RE = PosY + distance_RE * Math.cos((rotation * Math.PI) / 180);
+        ctx.beginPath();
+        ctx.moveTo(PosX, PosY);
+        ctx.lineTo(newX_RE, newY_RE);
+        ctx.stroke();
+        PosX = newX_RE;
+        PosY = newY_RE;
+        updateRocketPosition();
         break;
       case "TD":
         console.log(`La tortue tourne à droite de ${command.params[0]} degrés`);
+        rotation -= command.params[0];
+        rotation = (rotation + 360) % 360;
+        updateRocketPosition();
         break;
       case "TG":
         console.log(`La tortue tourne à gauche de ${command.params[0]} degrés`);
+        rotation += command.params[0];
+        rotation = rotation % 360;
+        updateRocketPosition();
         break;
       case "LC":
         console.log("La tortue ne laisse pas de trace");
+        updateRocketPosition();
         break;
       case "BC":
         console.log("La tortue laisse une trace");
+        updateRocketPosition();
         break;
       case "CT":
         console.log("La tortue est cachée");
+        updateRocketPosition();
+        RocketVisible = false;
         break;
       case "MT":
         console.log("La tortue est visible");
+        RocketVisible = true;
+        updateRocketPosition();
         break;
       case "VE":
         console.log("Efface l'écran et replace la tortue au centre");
+        ctx.fillRect(0, 0, 450, 450); // Initialise le fond du canvas en blanc
+        PosX = 225;
+        PosY = 225;
+        ctx.moveTo(PosX, PosY);
+        updateRocketPosition();
         break;
       case "NETTOIE":
         console.log("Efface l'écran sans changer la position de la tortue");
+        ctx.fillRect(0, 0, 450, 450); // Initialise le fond du canvas en blanc
+        ctx.moveTo(PosX, PosY);
         break;
       case "ORIGINE":
         console.log("Replace la tortue au centre");
+        PosX = 225;
+        PosY = 225;
+        ctx.moveTo(PosX, PosY);
+        updateRocketPosition();
         break;
       case "VT":
         console.log("Efface la console");
+        updateRocketPosition();
         break;
       case "FCC":
         console.log(`Change la couleur du trait en ${command.params[0]}`);
+        updateRocketPosition();
         break;
       case "FCB":
         console.log(`Change la couleur du fond en ${command.params[0]}`);
+        updateRocketPosition();
         break;
       case "FCAP":
         console.log(`Fixe l'angle de la tortue à ${command.params[0]} degrés`);
+        updateRocketPosition();
         break;
       case "CAP":
         console.log("Retourne l'angle de la tortue");
+        rotation += 180;
+        updateRocketPosition();
         break;
       case "FPOS":
         console.log(`Positionne la tortue au point (${command.params[0]}, ${command.params[1]})`);
+        updateRocketPosition();
         break;
       case "POSITION":
       case "POS":
         console.log("Retourne la position de la tortue");
+        updateRocketPosition();
         break;
       default:
         console.log(`Commande non reconnue: ${command.command}`);
+        updateRocketPosition();
     }
   };
 
@@ -179,7 +232,7 @@ const ConsoleDrawComponent: FC<Props> = () => {
         await parseAndExecuteLogoScript(procedure.body, 0, localParams);
       } else {
         const params = parts.map(Number).filter((param) => !isNaN(param));
-        simulateTurtle({ command, params }, ctx);
+        simulateRocket({ command, params }, ctx);
       }
 
       lineIndex++;
@@ -189,7 +242,9 @@ const ConsoleDrawComponent: FC<Props> = () => {
   return (
     <div className="flex-col g15 dark-bg dark-container display-from-left">
       <div className="canva-container relative">
-        <i className="material-icons absolute blue pointer">rocket</i>
+        <i className="material-icons absolute blue pointer" id="cursor">
+          rocket
+        </i>
         <canvas ref={canvasRef} width="450" height="450" id="viewDraft" />
       </div>
 
